c9088fda643c13f68f2b786dd963cf3729758c53
HP2708 <85988722+HP2708@users.noreply.github.com>
2021-06-16 23:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
+    var p,n,r,si;
     p = document.getElementById("principal").value;
-    
+    n = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+    si = (p * n * r) / 100;
+    var y=2021+n;
+    document.write("If you deposit " +p+"\n,at an interest of"+r+".\n You will recieve an amount of "+si+",\nin the year"+y);    
 }
-        
